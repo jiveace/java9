e3a1c514ac0dc33945f209f524605f5701340392
jiveace <straightedgescotsman@gmail.com>
2018-11-06 13:34:32 +0000

    Migration example code is now useless, so it has been DELETED! DELETE! DELETE! DELETE!

--- migrationExample/compile.sh
+++
@@ -1,6 +0,0 @@
-javac --class-path lib/gson-2.8.1.jar 	\
-	-d out $(find src -name '*.java')
-
-jar --create --file lib/person.jar \
-	-C out	.
-
--- migrationExample/run.bat
+++
@@ -1,3 +0,0 @@
-java --class-path lib\gson-2.8.1.jar;lib\person.jar  ^
-	academy.learnprogramming.main.Main
-
