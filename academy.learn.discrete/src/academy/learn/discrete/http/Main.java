1af520589bceafd06bcb516741cd359bfec92b71
jiveace <straightedgescotsman@gmail.com>
2018-11-07 15:38:57 +0000

    HTTP Client v2

---
+++ academy.learn.discrete/src/academy/learn/discrete/http/Main.java
@@ -0,0 +1,62 @@
+package academy.learn.discrete.http;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpHeaders;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String url = "http://www.example.com";
+
+        getURLSync(url);
+        postURLSync(url);
+    }
+
+    public static void getURLSync(String url) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest
+                .newBuilder(new URI(url))
+                .GET()
+                .build();
+
+        HttpResponse response = client.send(request, HttpResponse.BodyHandler.asString());
+        processResponse(response);
+    }
+
+    private static void processResponse(HttpResponse response) {
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println("Body: " + response.body());
+
+        printHeaders(response);
+    }
+
+    private static void printHeaders(HttpResponse response) {
+        System.out.println("Headers: ");
+        HttpHeaders headers = response.headers();
+        var map = headers.map();
+        map.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+
+    public static void postURLSync(String url) throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse response = client.send(
+                HttpRequest
+                    .newBuilder(new URI(url))
+                    .headers("Foo", "Foo-val", "bar", "Bar-val")
+                    .POST(HttpRequest.BodyPublisher.fromString("This is the String"))
+                    .build(),
+                HttpResponse.BodyHandler.asFile(Paths.get("fileFreddyFishFinger.txt"))
+        );
+
+        processResponse(response);
+
+    }
+}
